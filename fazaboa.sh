213125b302e8a24366bd8157573acaac5767e602
Lucas Teske <lucas@teske.com.br>
2020-08-26 00:08:03 -0300

    Conseguimos fazer o PHP ler a versão do protocolo do minecraft

---
+++ fazaboa.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+php -dxdebug.remote_enable=1 -dxdebug.remote_mode=req -dxdebug.remote_port=9000 -dxdebug.remote_host=127.0.0.1 -dxdebug.remote_connect_back=0 test.php
---
+++ test2.php
@@ -0,0 +1,5 @@
+<?php
+//phpinfo();
+
+
+var_dump($_SERVER);
